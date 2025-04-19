24b6fc57ea4ec4bc29dd7a4a6f6de780a7342c17
MariiaKpln <kpln.mary@gmail.com>
2025-04-19 18:01:23 +0300

    specific TS types

--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,28 @@
 import "./style.css";
-//JS standart types
-let a = 10; //infering type
-// a = "kuku" static types no allowed updating type
-let b: number;
-let s: string = "10";
-let bl: boolean;
-bl = !!s;
-let n: null = null;
-let obj: { a: string } = { a: "kuku" };
-if (obj) {
-  //always true
-}
-// obj.a = 10 not works
-obj.a = "10";
-const c = "a";
-obj[c];
-//alias
-type Person = { id: number; name: string; city: string; age: number };
-let person: Person = {
-  id: 123,
-  age: 25,
-  city: "LOd",
-  name: "Lox",
-};
-console.log(person.age);
 
-function createPerson(
-  id: number,
-  name: string,
-  city: string,
-  age: number
-): Person {
-  return { id, name, city, age };
+const Person = { id: number; name: string; city: string; age: number };
+//any - not recommended
+let a: any;
+a = 10;
+a = "kukhkht";
+a.a = 10;
+a = {};
+//unknown - safed any
+// const person: Person = (id: 1222, name: "Pizduk", age: 30, city: "Lox")
+function display(obj: unknown) {
+  typeof obj === "number" && obj++;
+  console.log(obj)
+  let sub: string;
+ typeof obj === "string" &&  (sub  = obj.substring(1, 2));
 }
+//index signature
+let obj: {[key: string]: string} = {};
+obj[1] = "10";
+obj[2.6] = "w"
+//arrays
+let ar: number[] = [11, 123, 10]
+// ar.push(...[1, 2])
 
-const person1: Person = createPerson(123, "Lox", "bobrovsk", 30);
-person1.age = 40;
-function displayPersonAge(person: Person): void {
-  console.log(person.age);
-}
-function displayPersonName(person: Person): void {
-  console.log(person.name);
-}
-function displayKuku(num: number): number {
-  console.log(num);
-  return 5;
-}
-let displayPersonField: (person: Person) => void;
-displayPersonField = displayPersonAge;
-displayPersonField(person1);
-// displayPersonField = displayKuku; //zaluppppa
-displayPersonField(person);
+
+
+
