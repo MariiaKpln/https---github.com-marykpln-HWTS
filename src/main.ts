c0ca20b5be1e2f13d7c488fd1eddd96c57fc8848
MariiaKpln <kpln.mary@gmail.com>
2025-04-20 12:35:39 +0300

    HW TS

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,50 @@
 import "./style.css";
 
-const Person = { id: number; name: string; city: string; age: number };
-//any - not recommended
-let a: any;
-a = 10;
-a = "kukhkht";
-a.a = 10;
-a = {};
-//unknown - safed any
-// const person: Person = (id: 1222, name: "Pizduk", age: 30, city: "Lox")
-function display(obj: unknown) {
-  typeof obj === "number" && obj++;
-  console.log(obj)
-  let sub: string;
- typeof obj === "string" &&  (sub  = obj.substring(1, 2));
+function displayNumberOccurrences(numbers: number[]): void {
+  //Create a frequency map
+  const frequencyMap: { [key: number]: number } = {};
+
+  for (const num of numbers) {
+    frequencyMap[num] = (frequencyMap[num] || 0) + 1;
+  }
+
+  //Convert map to array of tuples: [number, count]
+  const entries: [number, number][] = Object.entries(frequencyMap).map(
+    ([key, value]) => [parseInt(key), value]
+  );
+
+  entries.sort((a, b) => (b[1] !== a[1] ? b[1] - a[1] : a[0] - b[0]));
+
+  //Display result
+  for (const [num, count] of entries) {
+    console.log(`${num} => ${count}`);
+  }
 }
-//index signature
-let obj: {[key: string]: string} = {};
-obj[1] = "10";
-obj[2.6] = "w"
-//arrays
-let ar: number[] = [11, 123, 10]
-// ar.push(...[1, 2])
 
+function isAnagram(word: string, anagram: string) {
+  let res: boolean;
+  if (word.length !== anagram.length) {
+    res = false;
+  }
+  const map1 = frequencyMap(word);
+  const map2 = frequencyMap(anagram);
 
+  for (const char in map1) {
+    if (map1[char] !== map2[char]) res = false;
+  }
 
+  res = true;
+  return res;
+}
 
+const frequencyMap = (str: string): { [char: string]: number } => {
+  const map: { [char: string]: number } = {};
+  for (const char of str) {
+    map[char] = (map[char] || 0) + 1;
+  }
+  return map;
+};
+
+console.log(isAnagram("lalala", "aaalll"));
+
+displayNumberOccurrences([20, 3, 3, 20, 20, 1, 1]);
