ec86eda035e2db82c37623b830574c216e004143
MariiaKpln <kpln.mary@gmail.com>
2025-04-20 19:00:09 +0300

    enum introduction

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,21 @@
-import "./style.css";
-
-function displayNumberOccurrences(numbers: number[]): void {
-  //Create a frequency map
-  const frequencyMap: { [key: number]: number } = {};
-
-  for (const num of numbers) {
-    frequencyMap[num] = (frequencyMap[num] || 0) + 1;
-  }
-
-  //Convert map to array of tuples: [number, count]
-  const entries: [number, number][] = Object.entries(frequencyMap).map(
-    ([key, value]) => [parseInt(key), value]
-  );
-
-  entries.sort((a, b) => (b[1] !== a[1] ? b[1] - a[1] : a[0] - b[0]));
-
-  //Display result
-  for (const [num, count] of entries) {
-    console.log(`${num} => ${count}`);
-  }
+enum DayOfWeek {
+  Sunday,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sun = 0,
+  Mon,
+  Tue,
+  Wed,
+  Thu,
+  Fri,
+  Sat,
 }
-
-function isAnagram(word: string, anagram: string) {
-  let res: boolean;
-  if (word.length !== anagram.length) {
-    res = false;
-  }
-  const map1 = frequencyMap(word);
-  const map2 = frequencyMap(anagram);
-
-  for (const char in map1) {
-    if (map1[char] !== map2[char]) res = false;
-  }
-
-  res = true;
-  return res;
+let dayOfWeek1 = DayOfWeek.Sunday;
+let dayOfWeek2 = DayOfWeek.Sun;
+if (dayOfWeek1 === dayOfWeek2) {
+  console.log("The same day of the week");
 }
-
-const frequencyMap = (str: string): { [char: string]: number } => {
-  const map: { [char: string]: number } = {};
-  for (const char of str) {
-    map[char] = (map[char] || 0) + 1;
-  }
-  return map;
-};
-
-console.log(isAnagram("lalala", "aaalll"));
-
-displayNumberOccurrences([20, 3, 3, 20, 20, 1, 1]);
