2d06dbe340b33c368f81f6b071b6cfcc147c011d
MariiaKpln <kpln.mary@gmail.com>
2025-04-20 19:27:08 +0300

    union literal intersection  and keyof

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,27 @@
-enum DayOfWeek {
-  Sunday,
-  Monday,
-  Tuesday,
-  Wednesday,
-  Thursday,
-  Friday,
-  Saturday,
-  Sun = 0,
-  Mon,
-  Tue,
-  Wed,
-  Thu,
-  Fri,
-  Sat,
+//union
+function kgToGr(kg: number | string) {
+  //Narrowing type
+  if (typeof kg === "string") {
+    kg = parseFloat(kg);
+  }
+  return kg * 1000;
 }
-let dayOfWeek1 = DayOfWeek.Sunday;
-let dayOfWeek2 = DayOfWeek.Sun;
-if (dayOfWeek1 === dayOfWeek2) {
-  console.log("The same day of the week");
-}
+
+let a: 50 | 75 | 100 | "abc"; //a can have only 50, 75 or 100 or "abc"
+
+//intersection types
+type A = { a: string };
+type B = { b: number };
+type C = A & B; // C has both properties a and b
+let obj: C = { a: "hello", b: 42 };
+type D = string & number; // D is never, as str and num are incopatible tipes
+
+type Product = {
+  id: number;
+  name: string;
+  price: number;
+};
+
+let productKey: keyof Product; //id name or price
+productKey = "id" //valid 
+// productKey = "bubu" //unvalid
